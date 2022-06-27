e03a66086556b921e4f28441fe0aad1df84ad3c8
ManishGupta <guptamns3786@gmail.com>
2022-06-27 12:14:33 +0530

    minor changes

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,12 +1,11 @@
-const router = require('express').Router()
-const userCtrl = require('../controllers/userCtrl')
-const upload =require('../middleware/multer')
-const auth =require('../middleware/access')
+const router = require("express").Router();
+const userCtrl = require("../controllers/userCtrl");
+const upload = require("../middleware/multer");
+const auth = require("../middleware/access");
 
-router.post('/register',upload.single('image'), userCtrl.register)
-router.get('/getUser',auth,userCtrl.getUserById)
-router.post('/login',userCtrl.login)
-
-router.post('/newRequest',userCtrl.createNewRequest)
-router.get('/getAllRequest',userCtrl.getAllRequest)
-module.exports = router
+router.post("/register", upload.single("image"), userCtrl.register);
+router.get("/getUser", auth, userCtrl.getUserById);
+router.post("/login", userCtrl.login);
+router.post("/newRequest", userCtrl.createNewRequest);
+router.get("/getAllRequest", userCtrl.getAllRequest);
+module.exports = router;
